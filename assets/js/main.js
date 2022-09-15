71e126bb2b49da3ab3cf1a7ac7c0e3d9e39c4659
MdSyfulKhan <mdsyfulkhan9899@gmail.com>
2022-09-15 17:49:37 +0600

    2 page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+dynamicBackground();
+
+
 function showPassword(){
   var passwordField = $('#password');
   var passwordFieldType = $(passwordField).attr('type');
@@ -12,3 +15,20 @@ $('.st-showPassword').click(function(){
   showPassword();
   $(this).toggleClass('active');
 });
+
+$('.st-social-icons.st-style2 a').mouseenter(function(){
+  $(this).addClass('active').siblings().removeClass('active');
+});
+
+
+
+// dynamic bg 
+function dynamicBackground() {
+  // Background images
+  $('.st-dynamic-bg').each(function() {
+      var src = $(this).attr('data-src');
+      $(this).css({
+          'background-image': 'url(' + src + ')'
+      });
+  });
+}
